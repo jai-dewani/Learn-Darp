93aab4a4e2e5b8ceabbd6d6c2cbb0e5dc90f68e2
Jai Kumar Dewani <jai.dewani.99@gmail.com>
2024-09-23 20:19:44 +0530

    Console application to publish events

---
+++ pub-sub/checkout/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Dapr.Client;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+for (int i = 1; i <= 10; i++) {
+    var order = new Order(i);
+    using var client = new DaprClientBuilder().Build();
+
+    // Publish an event/message using Dapr PubSub
+    await client.PublishEventAsync("azureorderpubsub", "orders", order);
+    Console.WriteLine("Published data: " + order);
+    await client.PublishEventAsync("orderpubsub", "myNewOrders", i);
+    Console.WriteLine("Published new data format: " + i);
+
+    
+    await Task.Delay(TimeSpan.FromSeconds(1));
+}
+
+public record Order([property: JsonPropertyName("orderId")] int OrderId);
